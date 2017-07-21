96cf481dbd808124d53fd7e069ef7b874bfa0516
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2017-07-21 21:42:20 +0000

    Add single pair comparison

--- compSequentialEqual.h
+++ compSequentialEqual.h
@@ -1,4 +1,22 @@
 #ifndef COMP_SEQUENTIAL_EQUAL_H
 #define COMP_SEQUENTIAL_EQUAL_H
+
+namespace comp
+{
+    template<typename T>
+    bool sequentialEqual(T const & lhs, T const & rhs);
+}
+
+namespace comp
+{
+////////////////////////////////////////////////////////////////////////////////
+template<typename T>
+inline bool sequentialEqual(T const & lhs, T const & rhs)
+{
+    return (lhs == rhs);
+}
+////////////////////////////////////////////////////////////////////////////////
+}
+
 #endif /* COMP_SEQUENTIAL_EQUAL_H */
 
--- test/unittest/source/test_compSequentialEqual.cpp
+++ test/unittest/source/test_compSequentialEqual.cpp
@@ -2,3 +2,26 @@
 
 #include "compSequentialEqual.h"
 
+#include <string>
+
+
+TEST_CASE("sequentialEqual", "[comp][sequentialEqual")
+{
+    int const i1 = 1;
+    int const i2 = 2;
+    double const d1 = 3.415;
+    double const d2 = 7.111;
+    std::string const s1 = "Foo";
+    std::string const s2 = "Bar";
+
+    SECTION("one pair")
+    {
+        REQUIRE(comp::sequentialEqual(i1, i1));
+        REQUIRE(!comp::sequentialEqual(i1, i2));
+        REQUIRE(comp::sequentialEqual(d1, d1));
+        REQUIRE(!comp::sequentialEqual(d1, d2));
+        REQUIRE(comp::sequentialEqual(s1, s1));
+        REQUIRE(!comp::sequentialEqual(s1, s2));
+    }
+}
+

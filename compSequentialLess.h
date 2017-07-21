d04d151d223347b0668fa5198b06e8462d03bc9c
Krzysiek Karbowiak <17130511+kkarbowiak@users.noreply.github.com>
2017-07-21 22:48:08 +0000

    Extend comparison to any number of pairs

--- compSequentialLess.h
+++ compSequentialLess.h
@@ -5,6 +5,9 @@ namespace comp
 {
     template<typename T>
     bool sequentialLess(T const & lhs, T const & rhs);
+
+    template<typename H, typename ... Ts>
+    bool sequentialLess(H const & lhs, H const & rhs, Ts const & ... ts);
 }
 
 namespace comp
@@ -14,6 +17,23 @@ template<typename T>
 inline bool sequentialLess(T const & lhs, T const & rhs)
 {
     return (lhs < rhs);
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename H, typename ... Ts>
+inline bool sequentialLess(H const & lhs, H const & rhs, Ts const & ... ts)
+{
+    if (lhs < rhs)
+    {
+        return true;
+    }
+    else if (rhs < lhs)
+    {
+        return false;
+    }
+    else
+    {
+        return sequentialLess(ts ...);
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////
 }

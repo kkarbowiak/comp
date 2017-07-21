27d4878507f7ee38ad07befbc26dc62927b5ad46
kkarbowiak <kkarbowiak@localhost>
2017-07-21 22:19:06 +0000

    Add single pair comparison

--- compSequentialLess.h
+++ compSequentialLess.h
@@ -1,4 +1,22 @@
 #ifndef COMP_SEQUENTIAL_LESS_H
 #define COMP_SEQUENTIAL_LESS_H
+
+namespace comp
+{
+    template<typename T>
+    bool sequentialLess(T const & lhs, T const & rhs);
+}
+
+namespace comp
+{
+////////////////////////////////////////////////////////////////////////////////
+template<typename T>
+inline bool sequentialLess(T const & lhs, T const & rhs)
+{
+    return (lhs < rhs);
+}
+////////////////////////////////////////////////////////////////////////////////
+}
+
 #endif /* COMP_SEQUENTIAL_LESS_H */
 
